e59a6eeb8c911d2876d4f4656abfdffd06db3040
ivanrochaaragundy-design <ivan.rocha.aragundy@gmail.com>
2026-09-23 19:35:34 -0500

    Create reembolsos.js

---
+++ pages/reembolsos.js
@@ -0,0 +1,11 @@
+export default function Reembolsos() {
+  return (
+    <div style={{ maxWidth: 700, margin: "0 auto", padding: "60px 24px", fontFamily: "Georgia, serif", color: "#333", lineHeight: 1.7 }}>
+      <h1>Política de Reembolsos</h1>
+      <p><em>Última actualización: 23 de septiembre de 2026</em></p>
+      <p>Si no estás satisfecho con tu suscripción a Lunarcana, puedes solicitar un reembolso dentro de los 7 días posteriores al cargo, escribiendo a lunaarcana96@gmail.com con el correo asociado a tu cuenta.</p>
+      <p>Los reembolsos se procesan a través de Paddle y pueden tardar de 5 a 10 días hábiles en reflejarse, dependiendo de tu método de pago.</p>
+      <p>Las cancelaciones de suscripción no generan reembolso automático del período ya iniciado, salvo lo indicado arriba.</p>
+    </div>
+  );
+}
